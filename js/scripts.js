a36ecda2f414b3f39aa72c8c1fa1b58e3103f142
Dunya <duni2829@gmail.com>
2023-04-18 14:01:27 +0300

    root

--- js/scripts.js
+++ js/scripts.js
@@ -898,3 +898,13 @@ if (product) {
   }
 });
 }
+function set_100vh_var() {
+  document.documentElement.style.setProperty('--vh', `${window.innerHeight/100}px`);
+};
+window.addEventListener('resize', function () {
+  set_100vh_var();
+  detect_mobile();
+});
+window.addEventListener('orientationchange', function () {
+  set_100vh_var();
+});
