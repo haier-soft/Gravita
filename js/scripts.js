936031d6380bb0795da2822b98a80b1928dd33c8
Dunya <duni2829@gmail.com>
2023-04-18 18:20:13 +0300

    kk

--- js/scripts.js
+++ js/scripts.js
@@ -561,6 +561,7 @@ $(function () {
         if (window.innerWidth < 576) {
           delta = 180;
         }
+        $(".product__right").css({"top": window.innerHeight - delta});
         let productOffsetBottom =
           product.offset().top +
           product.find(".product__left").height() -
